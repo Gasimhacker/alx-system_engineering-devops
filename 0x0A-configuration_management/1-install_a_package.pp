29ae989cbf589e62a390af115e0b2049578428ef
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-22 23:12:56 +0000

    Use strings

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
 # Install flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
 }
 package { 'werkzeug':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
 }
