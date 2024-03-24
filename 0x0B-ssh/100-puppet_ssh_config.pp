8782457009b6095045b37202463decbdea68ead2
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-24 04:27:14 +0000

    Revert "Use the path as the title"
    This reverts commit b3b260e440fe6d7180c47e98233bed5aa90de14e.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # Edit the ssh configuration
 include stdlib
 
-file_line { '/etc/ssh/ssh_config':
+file_line { 'Only key_pairs':
   ensure  => present,
+  path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
 }
-file_line { '/etc/ssh/ssh_config':
+file_line { 'Change the location of the private_key':
   ensure  => present,
+  path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
 }
