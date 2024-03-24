6a5d73ada5b9177572c3c753597d9e37cf292b15
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-24 04:25:19 +0000

    Try removing the replace

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,9 @@ file_line { 'Only key_pairs':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
-  replace => true
 }
 file_line { 'Change the location of the private_key':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
-  replace => true
 }
