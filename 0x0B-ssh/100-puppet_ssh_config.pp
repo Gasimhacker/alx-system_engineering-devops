b3b260e440fe6d7180c47e98233bed5aa90de14e
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-24 04:26:20 +0000

    Use the path as the title

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 # Edit the ssh configuration
 include stdlib
 
-file_line { 'Only key_pairs':
+file_line { '/etc/ssh/ssh_config':
   ensure  => present,
-  path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
 }
-file_line { 'Change the location of the private_key':
+file_line { '/etc/ssh/ssh_config':
   ensure  => present,
-  path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
 }
