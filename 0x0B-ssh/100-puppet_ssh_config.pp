d75723735b9a2bf64632c4c66754f250eed7a964
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-24 04:17:30 +0000

    Use the replace attribute

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,12 @@
 include stdlib
 
 file_line { '/etc/ssh/ssh_config':
-  ensure => present,
-  line   => "	AuthenticatePassword no",
+  ensure  => present,
+  line    => "	AuthenticatePassword no",
+  replace => true
 }
 file_line { '/etc/ssh/ssh_config':
-  ensure => present,
-  line   => "	IdentityFile ~/.ssh/school",
+  ensure  => present,
+  line    => "	IdentityFile ~/.ssh/school",
+  replace => true
 }
