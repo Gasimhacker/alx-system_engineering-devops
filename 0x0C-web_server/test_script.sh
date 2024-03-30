86c069e60be05f34c261265347ffc29df182f0a9
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-30 02:48:13 +0000

    Configure the error page for the server

--- 0x0C-web_server/test_script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-redirection_rule="\n\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}"
-old_string="server_name _";
-sed -i "/$old_string/a \ $redirection_rule" /etc/nginx/sites-available/default
