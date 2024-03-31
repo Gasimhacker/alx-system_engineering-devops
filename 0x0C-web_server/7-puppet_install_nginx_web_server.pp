494aea0ea73be84290d825113a235d5235541869
Mohammed Ahmed <ma2286572@gmail.com>
2024-03-31 04:41:10 +0000

    Edit the configuration structure

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,44 +1,20 @@
-# a script that add stable version of nginx
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+# Automate the setup of nginx server
 
-# update software packages list
 exec { 'update packages':
   command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# install nginx
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',}
 package { 'nginx':
   ensure     => 'installed',
 }
 
-# allow HTTP
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
-}
-
-# change folder rights
-exec { 'chmod www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# create index file
 file { '/var/www/html/index.html':
   content => "Hello World!\n",
 }
 
-# create index file
 file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
 }
 
-# add redirection and error page
 file { 'Nginx default config file':
   ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
@@ -50,6 +26,9 @@ file { 'Nginx default config file':
         # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
         server_name _;
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
         location / {
                 # First attempt to serve request as file, then
                 # as directory, then fall back to displaying a 404.
@@ -57,22 +36,17 @@ file { 'Nginx default config file':
         }
         error_page 404 /404.html;
         location  /404.html {
-            internal;
-        }
-        
-        if (\$request_filename ~ redirect_me){
-            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-        }
+            internal;        
+	}
 }
 ",
 }
-# restart nginx
+
 exec { 'restart service':
   command => 'service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-# start service nginx
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
