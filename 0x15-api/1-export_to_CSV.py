94da50953a33ca75f801a62b22252fd4eda8f1fe
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-25 01:21:05 +0000

    Use the correct file name

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,21 +6,19 @@ about progress for an employee
 
 if __name__ == "__main__":
     import csv
-    from json import loads
+    import requests
     from sys import argv
-    from urllib.request import urlopen
 
-    base_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
-    with urlopen(base_url) as emp_data:
-        name = loads(emp_data.read())['username']
-    with urlopen(f'{base_url}/todos') as todos:
-        todo_list = loads(todos.read())
+    url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    username = requests.get(url).json()['username']
+    todo_list = requests.get(f'{url}/todos').json()
 
-    with open('USER_ID.csv', 'w', newline='') as csvfile:
+    with open(f'{argv[1]}.csv', 'w', newline='') as csvfile:
         fieldnames = ['userId', 'username', 'completed', 'title']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
         for task in todo_list:
             writer.writerow({'userId': task['userId'],
-                             'username': name,
+                             'username': username,
                              'completed': task['completed'],
                              'title': task['title']})
