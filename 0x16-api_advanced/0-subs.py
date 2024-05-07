6c45c9243f0801bc3d9a168d479d2ce5452e07e5
Mohammed Ahmed <ma2286572@gmail.com>
2024-05-07 09:08:23 +0000

    Ensure that the attribute is there

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Chrome/124.0.0.0 Safari/537.36'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     r = requests.get(url, headers=headers)
-    if (r.status_code == 200):
-        return r.json()['data']['subscribers']
-    else:
+    if (r.status_code == 404):
         return 0
+    subreddit_data = r.json().get('data')
+    return subreddit_data.get('subscribers')
