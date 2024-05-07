561cd3f122d513abb73ffcc8010628915c3e79e0
Mohammed Ahmed <ma2286572@gmail.com>
2024-05-07 09:37:13 +0000

    Disable redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """Print the title of the top 10 hot posts"""
     headers = {'User-Agent': 'Chrome/124.0.0.0 Safari/537.36'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, allow_redirects=False)
     if (r.status_code == 200):
         hot_list = r.json()['data']['children']
         [print(post['data']['title']) for post in hot_list]
