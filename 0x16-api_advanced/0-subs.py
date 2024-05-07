0e677e26550795b82bcf3ed7267dd72f7c263730
Mohammed Ahmed <ma2286572@gmail.com>
2024-05-07 09:20:27 +0000

    Try this temp file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""A script to count the number of subscribers in a subreddit"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Count the number of subscribers"""
-    headers = {'User-Agent': 'Chrome/124.0.0.0 Safari/537.36'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if (r.status_code == 404):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    subreddit_data = r.json().get('data')
-    return subreddit_data.get('subscribers')
+    results = response.json().get("data")
+    return results.get("subscribers")
