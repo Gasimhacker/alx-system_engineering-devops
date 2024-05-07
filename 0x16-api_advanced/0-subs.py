955db3aa926592de87847832916594aee030aa4e
Mohammed Ahmed <ma2286572@gmail.com>
2024-05-07 09:15:28 +0000

    Revert "Change the user agent"
    This reverts commit 9e3cc246cb6d203f848f2b0320afcab026a65643.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Count the number of subscribers"""
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+    headers = {'User-Agent': 'Chrome/124.0.0.0 Safari/537.36'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     r = requests.get(url, headers=headers, allow_redirects=False)
     if (r.status_code == 404):
